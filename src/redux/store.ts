58cc37a517fe3bc0ae5edc1d3d28125ef5b3b3c6
armcruz <jaimes087.uz@gmail.com>
2021-08-10 19:39:47 -0500

    fix: redux devtools disable in production

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -60,7 +60,9 @@ export interface RootState {
 }
 
 const composeEnhancers =
-	(window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
+	process.env.NODE_ENV === 'development'
+		? (window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']
+		: null || compose;
 
 const enhancers = composeEnhancers(applyMiddleware(thunk));
 
